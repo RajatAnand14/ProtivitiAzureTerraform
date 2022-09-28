a8493c25c80f8aececfa3109b040315943a3a9af
RajatAnand <rajana01@protiviti.com>
2022-09-27 23:03:03 -0500

    ssh into linux server

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_network_security_group" "azurensg" {
   }
   security_rule {
     name                       = "TCPSMTP"
-    priority                   = 500
+    priority                   = 600
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
